9fb0fbc5a1366f43de5f0f21f8f6d9c1acf0f3f0
mhdafnan2003 <mhdafnan2003@gmail.com>
2024-12-09 00:07:24 +0530

    third

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import 'package:flutter/material.dart';
 import 'package:tezos/Screens/MainScreen.dart';
 
@@ -18,3 +19,25 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+=======
+import 'package:flutter/material.dart';
+import 'package:tezos/Screens/MainScreen.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: Mainscreen(),
+    );
+  }
+}
+>>>>>>> 7b5ebc826356e6fd6e1bed546c5c63959e89a441
